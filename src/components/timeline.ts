31bdeddebb6ccb5a46b9e7c863f58d643545098d
Robin Backhaus <backhaus.robin@gmail.com>
2018-06-07 16:45:40 +0200

    timeline element

---
+++ src/components/timeline.ts
@@ -0,0 +1,7 @@
+export class TimelineCustomElement {
+  // public counters = [
+  //   {start: 40, end: 60, color: 'green'},
+  //   {start: 20, end: 40, color: 'blue'},
+  //   {start: 0, end: 20, color: 'red'}
+  // ];
+}
