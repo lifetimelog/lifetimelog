33347e020273e3c8e296b13a38031ac9148b96cf
Robin Backhaus <backhaus.robin@gmail.com>
2018-06-12 17:06:57 +0200

    timeline now has a scale

--- src/components/timeline.ts
+++ src/components/timeline.ts
@@ -1,17 +1,33 @@
 import { autoinject } from 'aurelia-framework';
 import { StateService } from 'services/state-service';
-import { SECOND, MINUTE, HOUR, DAY } from 'lib/constants';
+import { MINUTE, HOUR, DAY } from 'lib/constants';
 
 @autoinject
 export class TimelineCustomElement {
 
-  private today;
-  private nextHour;
+  private timezoneOffset = new Date().getTimezoneOffset();
+  private today = Math.floor(Date.now() / DAY) * DAY + this.timezoneOffset * MINUTE;
+  private clockInterval;
+  private now = Date.now();
+  private scale = 30000;
+  private timelineElement;
+  private markers = [];
 
   constructor(
     private stateService: StateService
   ) {
-    this.today = Math.floor(Date.now() / DAY) * DAY;
-    this.nextHour = Date.now() + HOUR;
+    this.clockInterval = setInterval(() => this.now = Date.now(), 1000);
+    for (let i = 0; i <= 96; i++) {
+      this.markers.push({
+        time: i / 4 * HOUR,
+        text: i % 4 === 0 ? ((i / 400) + '0').slice(2, 4) + ':00' : '',
+        color: i % 4 === 0 ? 'black' : 'lightgrey'
+      });
+    }
   }
+
+  private attached() {
+    this.timelineElement.scrollLeft = DAY / this.scale;
+  }
+
 }
--- src/services/state-service.ts
+++ src/services/state-service.ts
@@ -1,4 +1,4 @@
-import { MINUTE } from 'lib/constants';
+import { DAY, HOUR, MINUTE } from 'lib/constants';
 import { EventAggregator } from 'aurelia-event-aggregator';
 
 import { autoinject } from 'aurelia-framework';
@@ -20,6 +20,7 @@ interface IState {
 export class StateService {
 
   public state: IState;
+  private today = Math.floor(Date.now() / DAY) * DAY + new Date().getTimezoneOffset() * MINUTE;
 
   constructor(
     private httpClient: HttpClient,
@@ -27,31 +28,32 @@ export class StateService {
   ) {
     this.state = {timers: [], counters: []};
     this.state.timers = [
-        Object.assign(new Timer(), {guid: '0001', name: 'a counter', color: 'teal'}),
-        Object.assign(new Timer(), {guid: '0002', name: 'a counter 2', color: 'red'}),
-        Object.assign(new Timer(), {guid: '0003', name: 'a counter 3', color: 'blue'}),
-        Object.assign(new Timer(), {guid: '0004', name: 'a counter 4', color: 'green'})
-      ];
+      Object.assign(new Timer(), {guid: '0001', name: 'a counter', color: 'teal'}),
+      Object.assign(new Timer(), {guid: '0002', name: 'a counter 2', color: 'red'}),
+      Object.assign(new Timer(), {guid: '0003', name: 'a counter 3', color: 'blue'}),
+      Object.assign(new Timer(), {guid: '0004', name: 'a counter 4', color: 'green'})
+    ];
+
     this.state.counters = [
       new Counter({
         timer: this.state.timers[0],
-        start: Date.now() - 80 * MINUTE,
-        end: Date.now() - 60 * MINUTE,
+        start: this.today + 7 * HOUR + 15 * MINUTE,
+        end: this.today + 7 * HOUR + 45 * MINUTE,
         notice: 'some note'}),
       new Counter({
         timer: this.state.timers[1],
-        start: Date.now() - 60 * MINUTE,
-        end: Date.now() - 40 * MINUTE,
+        start: this.today + 9 * HOUR,
+        end: this.today + 11 * HOUR,
         notice: 'some note'}),
       new Counter({
         timer: this.state.timers[2],
-        start: Date.now() - 40 * MINUTE,
-        end: Date.now() - 20 * MINUTE,
+        start: this.today + 11 * HOUR,
+        end: this.today + 11 * HOUR + 15 * MINUTE,
         notice: 'some note'}),
       new Counter({
         timer: this.state.timers[3],
-        start: Date.now() - 20 * MINUTE,
-        end: Date.now(),
+        start: this.today + 11 * HOUR + 15 * MINUTE,
+        end: this.today + 11 * HOUR + 45 * MINUTE,
         notice: 'some note'})
     ];
   }
